62d7d3ebc48572d4fc8c0f30a5170751d4e1ffbe
Riverrushz <90576652+Riverrushz@users.noreply.github.com>
2026-01-27 03:16:23 -0500

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -76,9 +76,28 @@ class Lesson(models.Model):
     content = models.TextField()
 
 
-# Enrollment model
-# <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
-# And we could use the enrollment to track information such as exam submissions
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete = models.CASCADE)
+    text = models.CharField(max_length=200)
+    grade = models.IntegerField(default = 50) 
+
+    def __str__(self):
+        return "Question: " + self.content
+
+        
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    content = models.CharField(max_length=200)
+    correctChoice = models.BooleanField(default=False)
+    
 class Enrollment(models.Model):
     AUDIT = 'audit'
     HONOR = 'honor'
@@ -93,6 +112,14 @@ class Enrollment(models.Model):
     date_enrolled = models.DateField(default=now)
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment,on_delete = models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+
+# <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
+# And we could use the enrollment to track information such as exam submissions
+
 
 
 # One enrollment could have multiple submission
